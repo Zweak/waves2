9afc3494de237faa84c10b0542e6774e2e155d1e
Zeka <83528337+Zweak@users.noreply.github.com>
2024-06-27 12:20:01 +0530

    rebuilt V1 cuz API changed, also uses vite now instead of create-react

---
+++ src/data.js
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    // {
+    //   name: "Ref",
+    //   cover: "",
+    //   artist: "",
+    //   audio: "",
+    //   color: ["", ""],
+    //   id: uuidv4(),
+    //   active: false
+    // },
+    {
+      name: "Jazz Cabbage ",
+      cover: "https://cms.chillhop.com/?serve&file=6506&size=full",
+      artist: "Strehlow, Ian Ewing",
+      audio: "https://stream.chillhop.com/mp3/9363",
+      color: ["#442E4B", "#EED0C0"],
+      id: uuidv4(),
+      active: true
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://cms.chillhop.com/?serve&file=6799&size=full",
+      artist: "Aiguille",
+      audio: "https://stream.chillhop.com/mp3/9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://cms.chillhop.com/?serve&file=12212&size=full",
+      artist: "Swørn",
+      audio: "https://stream.chillhop.com/mp3/15223",
+      color: ["#335396", "#2A2247"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://cms.chillhop.com/?serve&file=6799&size=full",
+      artist: "Aiguille",
+      audio: "https://stream.chillhop.com/mp3/9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://cms.chillhop.com/?serve&file=6816&size=full",
+      artist: "Swørn",
+      audio: "https://stream.chillhop.com/mp3/9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://cms.chillhop.com/?serve&file=10010&size=full",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://stream.chillhop.com/mp3/10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Carefree ",
+      cover:
+        "https://cms.chillhop.com/?serve&file=6825&size=full",
+      artist: "dryhope, Gustav Gustav",
+      audio: "https://stream.chillhop.com/mp3/9311",
+      color: ["#421D2E", "#E88D92"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Zenith",
+      cover: "https://cms.chillhop.com/?serve&file=9929&size=full",
+      artist: "Leavv",
+      audio: "https://stream.chillhop.com/mp3/9927",
+      color: ["#587062", "#21504A"],
+      id: uuidv4(),
+      active: false
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
